b0e38e21ae19b983f264e5d495db2cec06145cc9
GajdosGabriel <gajdosgabo@gmail.com>
2021-02-18 06:30:47 +0100

    Navigation ref

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,28 +1,28 @@
 <nav>
-    <div class="nav-content">
-        <ul>
-            <li class="navbar-nav">
-                <a class="navbar-brand" href="{{ url('/') }}">
+    <div style="max-width: 114rem" class="flex mx-auto justify-between py-2 flex-wrap">
+        <ul class="my-2">
+            <li>
+                <a href="{{ url('/') }}">
                     Hlas Cirkvi
-                    {{--{{ config('app.name', 'Laravel') }}--}}
-                    {{--                    <img src="{{ url('images/logo1.gif') }}" style="height: 4rem" alt="Logo traja králi">--}}
                 </a>
             </li>
         </ul>
 
-        <ul class="center">
+        <ul class="my-2">
             <li>
-                <a href="{{ route('online-prenosy') }}">Nedeľné prenosy</a>
-                @if( session()->has('countUnwatchedVideos') )
-                    <span
-                        style="color: white; background: red; padding: 0.2rem 0.5rem;margin-left: -.2rem; font-size: 80%; border-radius: 2rem">
-                        {{ session()->get('countUnwatchedVideos') }}</span>
-                @endif
+                <a class="flex items-center" href="{{ route('online-prenosy') }}">Nedeľné prenosy
+
+                    @if( session()->has('countUnwatchedVideos') )
+                        <div class="w-7 h-7 bg-red-500 text-white rounded-full flex justify-center items-center ml-1">
+                            {{ session()->get('countUnwatchedVideos') }}3
+                        </div>
+                    @endif
+                </a>
             </li>
             <li><a href="{{ route('konferencie.pute') }}">Vzdelávanie</a></li>
             {{--<li><a href="{{ route('zdravie') }}">Zdravie z Božej ruky</a></li>--}}
         </ul>
-        <ul class="center">
+        <ul class="my-2">
             {{--            <li><a href="{{ route('user.index') }}">Osobnosti</a></li>--}}
             <article-admin inline-template>
                 <div>
@@ -33,26 +33,40 @@
                         </a>
                     </li>
 
-                    <ul v-cloak v-if="all" @click="toggle()" class="dropdown-menu">
-                        <li><a title="Rádio 7 Slovenská redakcia" class="dropdown-item" href="#"
-                               onClick="window.open('http://radio7.sk/live-vysielanie','pagename','resizable,height=500,width=470')">Rádio
-                                7 sk
-                                <img style="height: 16px;margin-bottom: -3px;" src="{{ asset('images/flag-sk.jpg') }}">
+                    <ul v-cloak v-if="all" @click="toggle()"
+                        class="absolute z-10 flex flex-col bg-white rounded-md border-2 border-gray-500 text-gray-700">
+                        <li class="p-2 px-6 hover:bg-gray-300">
+                            <a title="Rádio 7 Slovenská redakcia" class="hover:text-gray-700 no-underline" href="#"
+                               onClick="window.open('http://radio7.sk/live-vysielanie','pagename','resizable,height=500,width=470')">
+                                <div class="flex">
+                                    Rádio 7 sk
+                                    <img class="h-8 ml-2" src="{{ asset('images/flag-sk.jpg') }}">
+                                </div>
+                            </a>
+                        </li>
+                        <li class="p-2 px-6 hover:bg-gray-300">
+                            <a title="Rádio 7 Česká redakcia" class="hover:text-gray-700 no-underline" href="#"
+                               onClick="window.open('http://listen.play.cz/player.html?shortcut=radio7&format=&v=20200120','pagename','resizable,height=500,width=470')">
+                                <div class="flex">
+                                    Rádio 7 cz
+                                    <img class="h-8 ml-2" src="{{ asset('images/flag-cz.jpg') }}">
+                                </div>
                             </a></li>
-                        <li><a title="Rádio 7 Česká redakcia" class="dropdown-item" href="#"
-                               onClick="window.open('http://listen.play.cz/player.html?shortcut=radio7&format=&v=20200120','pagename','resizable,height=500,width=470')">Rádio
-                                7 cz
-                                <img style="height: 16px;margin-bottom: -3px;" src="{{ asset('images/flag-cz.jpg') }}">
-                            </a></li>
-                        <li><a class="dropdown-item" href="#"
+                        <li class="p-2 px-6 hover:bg-gray-300">
+                            <a class="hover:text-gray-700 no-underline" href="#"
                                onClick="window.open('https://www.lumen.sk/radio-streaming.html?liveplayer=1','pagename','resizable,height=500,width=470')">Rádio
-                                Lumen</a></li>
-                        <li><a class="dropdown-item" href="#"
+                                Lumen</a>
+                        </li>
+                        <li class="p-2 px-6 hover:bg-gray-300">
+                            <a class="hover:text-gray-700 no-underline" href="#"
                                onClick="window.open('https://www.lumen.sk/radio-streaming.html?liveplayer=2','pagename','resizable,height=500,width=470')">Lumen
-                                Gospel</a></li>
-                        <li><a class="dropdown-item" href="#"
+                                Gospel</a>
+                        </li>
+                        <li class="p-2 px-6 hover:bg-gray-300">
+                            <a class="hover:text-gray-700 no-underline" href="#"
                                onClick="window.open('https://dreamsiteradioplayer.it/plrm/rmslovakia/','pagename','resizable,height=500,width=470')">Rádio
-                                Mária</a></li>
+                                Mária</a>
+                        </li>
                     </ul>
                 </div>
             </article-admin>
@@ -62,7 +76,7 @@
             </li>
         </ul>
 
-        <ul>
+        <ul class="my-2">
             @guest
                 <li><a href="{{ route('login') }}">{{ __('auth.login') }}</a></li>
                 {{--<li><a href="{{ route('register') }}">{{ __('auth.Register') }}</a></li>--}}
@@ -70,7 +84,7 @@
 
                 <bell></bell>
 
-                <ul class="nav-item dropdown">
+                <ul class="">
 
                     <article-admin inline-template>
                         <div class="relative">
@@ -96,9 +110,10 @@
                                     </li>
                                 @endcan
                                 <li class="p-2 px-6 hover:bg-gray-300">
-                                    <a class="hover:text-gray-700" href="{{ route('organization.profile', [auth()->user()->org_id, auth()->user()->slug]) }}">Profil</a>
+                                    <a class="hover:text-gray-700"
+                                       href="{{ route('organization.profile', [auth()->user()->org_id, auth()->user()->slug]) }}">Profil</a>
                                 </li>
-{{--                                <li title="divider"></li>--}}
+                                {{--                                <li title="divider"></li>--}}
                                 <li class="p-2 px-6 hover:bg-gray-300 hover:text-gray-700">
                                     <a class="hover:text-gray-700" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
@@ -110,7 +125,7 @@
                         </div>
                     </article-admin>
 
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
                         @csrf
                     </form>
                     @endguest
