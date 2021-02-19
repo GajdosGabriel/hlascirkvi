1ecefb8ce9b963b38684b5a5bba3710e7e1983a1
GajdosGabriel <gajdosgabo@gmail.com>
2021-02-19 15:37:40 +0100

    Denné čítanie

--- resources/views/verses/show.blade.php
+++ resources/views/verses/show.blade.php
@@ -1,40 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container mx-auto p-2">
 
+    <div class="page-title mb-4">
+        <h1 class="text-2xl font-semibold">{{ $post->title }}</h1>
+        <span class="font-semibold"> Zamyslenie na {{ Carbon\Carbon::now()->format('d M Y') }}</span>
+    </div>
 
-    {{--<video-item></video-item>--}}
-
-    {{--<youtube-dash></youtube-dash>--}}
-
-    {{--<example-component></example-component>--}}
-
-    <div class="page">
-        <div class="page-content">
-
-            <div class="page-title">
-                <h1>{{ $post->title }}</h1>
-                <div> Zamyslenie na {{ Carbon\Carbon::now()->format('d M Y') }}</div>
-            </div>
+    <div class="flex">
+        <div class="mx-2 w-5/12 flex flex-col justify-between">
 
             <div class="two-collums"> {!! $post->zamyslenie !!}<p>{{ $post->autor }}</p></div>
 
-            <div class="nav-button">
+            <div class="flex justify-between m-4">
                 @if($previous)
-                    <a class="btn" href="{{ URL::to( 'zamyslenia/' . $previous ) }}"><< Včera</a>
+                    <a class="hover:bg-gray-100 px-2 rounded" href="{{ URL::to( 'zamyslenia/' . $previous ) }}"><< Včera</a>
                 @endif
 
                 @if($next)
-                    <a class="btn" href="{{ URL::to( 'zamyslenia/' . $next ) }}">Zajtra >></a>
+                    <a class="hover:bg-gray-100 px-2 rounded" href="{{ URL::to( 'zamyslenia/' . $next ) }}">Zajtra >></a>
                 @endif
             </div>
         </div>
 
 
-        <div class="page-aside">
-            <div class="card">
-                <div class="card-header">Verš starej zmluvy</div>
+        <div class="mx-4 w-3/12 flex flex-col justify-between">
+            <div class="border-2 p-3 rounded-md shadow-md">
+                <div class="font-semibold">Verš starej zmluvy</div>
 
                 <div class="card-body">
                     <blockquote>
@@ -44,8 +37,8 @@
                 </div>
             </div>
 
-            <div class="card">
-                <div class="card-header">Verš novej zmluvy</div>
+            <div class="border-2 p-3 rounded-md shadow-md">
+                <div class="font-semibold">Verš novej zmluvy</div>
 
                 <div class="card-body">
                     <blockquote>
@@ -54,9 +47,9 @@
                     </blockquote>
                 </div>
             </div>
-
-            <img src="{{ asset('images/biblia1.jpg' ) }}">
         </div>
+
+        <img class="rounded-md" src="{{ asset('images/biblia1.jpg' ) }}">
 
     </div>
 
