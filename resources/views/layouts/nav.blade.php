e52ddaa61b5dd0b265e71c1970e21e0f4eed8903
GajdosGabriel <gajdosgabo@gmail.com>
2021-03-14 15:05:51 +0100

    nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -8,21 +8,24 @@
             <li>
                 <a class="flex items-center" href="{{ route('online-prenosy') }}">Nedeľné prenosy
 
-                    @if( session()->has('countUnwatchedVideos') )
+                    @if (session()->has('countUnwatchedVideos'))
                         <div class="w-5 h-5 bg-red-500 text-white rounded-full flex justify-center items-center ml-1">
-                           <span class="pb-1">{{ session()->get('countUnwatchedVideos') }}</span>
+                            <span class="pb-1">{{ session()->get('countUnwatchedVideos') }}</span>
                         </div>
                     @endif
                 </a>
             </li>
             <li><a href="{{ route('konferencie.pute') }}">Vzdelávanie</a></li>
-            {{--<li><a href="{{ route('zdravie') }}">Zdravie z Božej ruky</a></li>--}}
+            {{-- <li><a href="{{ route('zdravie') }}">Zdravie z Božej ruky</a></li> --}}
         </ul>
 
         <ul class="my-2 flex space-x-4">
             <radio-button></radio-button>
-            <li class="border-2 rounded-md px-2 whitespace-nowrap"><a href="{{ route('modlitby.index') }}" class="nav-link radio">
-                    <i class="fas fa-praying-hands mr-2 text-gray-300"></i>Modlitby</a>
+            <li class="border-2 rounded-md px-2 whitespace-nowrap">
+                <a href="{{ route('modlitby.index') }}" class="nav-link radio">
+                    <i class="fas fa-praying-hands mr-2 text-gray-300"></i>
+                    Modlitby
+                </a>
             </li>
             <li><a href="{{ route('event.index') }}">Podujatia</a></li>
         </ul>
@@ -30,7 +33,7 @@
         <ul class="my-2">
             @guest
                 <li><a href="{{ route('login') }}">{{ __('auth.login') }}</a></li>
-                {{--<li><a href="{{ route('register') }}">{{ __('auth.Register') }}</a></li>--}}
+                {{-- <li><a href="{{ route('register') }}">{{ __('auth.Register') }}</a></li> --}}
             @else
 
                 <bell></bell>
@@ -41,17 +44,16 @@
                         <div class="relative z-10">
                             <a id="navbarDropdown" class="nav-link radio" href="#">
                                 <li @click="toggle" class="whitespace-nowrap">
-                                <span
-                                    href="{{ route('organization.profile', [auth()->user()->org_id, auth()->user()->slug]) }}"
-                                    class="nav-link" href="#">
-                                    {{ auth()->user()->fullname }}
-                                </span>
+                                    <span
+                                        href="{{ route('organization.profile', [auth()->user()->org_id, auth()->user()->slug]) }}"
+                                        class="nav-link" href="#">
+                                        {{ auth()->user()->fullname }}
+                                    </span>
                                     <i class="fas fa-caret-down"></i>
                                 </li>
                             </a>
 
-                            <ul v-cloak v-if="open" @click="toggle"
-                                class="dropdown-menu">
+                            <ul v-cloak v-if="open" @click="toggle" class="dropdown-menu">
                                 @can('admin')
                                     <a href="{{ route('admin.home') }}">
                                         <li class="dropdown-item">
@@ -59,16 +61,16 @@
                                         </li>
                                     </a>
                                 @endcan
-                                <a href="{{ route('organization.profile', [auth()->user()->org_id, auth()->user()->slug]) }}">
+                                <a
+                                    href="{{ route('organization.profile', [auth()->user()->org_id, auth()->user()->slug]) }}">
                                     <li class="dropdown-item">
                                         Profil
                                     </li>
                                 </a>
-                                {{--   <li title="divider"></li>--}}
+                                {{-- <li title="divider"></li> --}}
                                 <li class="dropdown-item">
-                                    <a href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
+                                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
                                         {{ __('web.Logout') }}
                                     </a>
                                 </li>
@@ -79,8 +81,8 @@
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
                         @csrf
                     </form>
-                    @endguest
-                </ul>
+                @endguest
+            </ul>
 
 
         </ul>
