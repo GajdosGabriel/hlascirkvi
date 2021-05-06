dfd9bbd1a05168db75b392b41d932d4ed6bceef5
Gabriel Gajdoš <gajdosgabo@gmail.com>
2021-05-06 07:15:18 +0200

    css events

--- resources/views/emails/image_post.blade.php
+++ resources/views/emails/image_post.blade.php
@@ -1,13 +1,13 @@
 @forelse($post->images as $image)
-<img alt="{{ $post->organization->title }} / {{ $post->title }}"
-    src="{{ url($image->ThumbImageUrl) }}" style="width: 100px; float: left; margin-right: 10px ">
+    <img alt="{{ $post->organization->title }} / {{ $post->title }}" src="{{ url($image->ThumbImageUrl) }}"
+        style="width: 100px; float: left; margin-right: 10px ">
 @break
 @empty
-@if (isset($post->user->avatar))
-<img src="{{ Storage::url($post->user->userPictureUrl()) }}"
-    alt="{{ $post->organization->title }}" style="min-width: 260px">
-@else
-<img src="{{ asset('images/foto.jpg') }}" alt="{{ $post->organization->title }}"
-    style="min-width: 260px">
-@endif
-@endforelse
+    @if (isset($post->user->avatar))
+        <img src="{{ Storage::url($post->user->userPictureUrl()) }}" alt="{{ $post->organization->title }}"
+            style="width: 100px; float: left; margin-right: 10px ">
+    @else
+        <img src="{{ asset('images/foto.jpg') }}" alt="{{ $post->organization->title }}"
+            style="width: 100px; float: left; margin-right: 10px ">
+    @endif
+    @endforelse
--- resources/views/emails/posts.blade.php
+++ resources/views/emails/posts.blade.php
@@ -12,12 +12,46 @@
         <p>Zoradené podľa počtu sledovaní.</p>
 
     </div>
+{{-- Posts --}}
+    <div style="background: white; padding-bottom:2px; max-width: 700px; margin:auto; border-bottom: 5px solid rgb(133, 130, 130);
+                border-color: rgb(23, 6, 117); margin-bottom: 10px;">
 
-    <div style=" background: white; padding-bottom:2px; max-width: 700px; margin:auto; border-style: solid;
-    border-color: rgb(23, 6, 117); margin-bottom: 10px;">
-
+        <h2 style="padding-left:10px; padding-top:10px ">Najsledovanejšie videa</h2>
         {{-- Image --}}
         @forelse($posts as $post)
+            <div style=" padding:10px  ">
+
+                @include('emails.image_post')
+
+                <div style="margin-top: -.8rem">
+                    <a style="text-decoration: none" href="{{ route('post.show', [$post->id, $post->slug]) }}">
+                        <h2 style="font-size: 100%" title="{{ $post->title }}">
+                            {{ $post->title }}
+                        </h2>
+                    </a>
+                </div>
+
+                <div style="margin-top: -10px; font-size: 90%; margin-bottom:20px ">Autor:
+                    {{ $post->organization->title }}
+                </div>
+
+            </div>
+        @empty
+            bez záznamu
+        @endforelse
+
+
+    </div>
+
+
+
+    {{-- Events --}}
+    <div style="background: white; padding-bottom:2px; max-width: 700px; margin:auto; border-bottom: 5px solid rgb(133, 130, 130);
+    border-color: rgb(23, 6, 117); margin-bottom: 10px;">
+
+        <h2 style="padding-left:10px; padding-top:10px "> Pozvánky na podujatia</h2>
+        {{-- Image --}}
+        @forelse($events as $post)
             <div style=" padding:10px  ">
 
                 @include('emails.image_post')
