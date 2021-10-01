87d335ec5d72907c2434b0da920193abb1227bff
Gabriel Gajdoš <gajdosgabo@gmail.com>
2021-10-01 20:57:07 +0200

    Event prihlásenie iba registrovaný

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -19,7 +19,7 @@ class FavoritesController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth')->except('update');
+        $this->middleware('auth');
     }
 
     public function update(FavoriteRequest $request, $favorite)
