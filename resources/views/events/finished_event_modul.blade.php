e5001ca5abec783510dbcf6dd72320ed1329930b
Gabriel Gajdoš <gajdosgabo@gmail.com>
2022-01-09 15:19:28 +0100

    Small fix

--- resources/views/events/finished_event_modul.blade.php
+++ resources/views/events/finished_event_modul.blade.php
@@ -1,9 +1,16 @@
 <div style="margin-bottom: 3rem">
     <h5>
-        <a href="{{ route('event.finished') }}">
-            Ukončené akcie
-        </a>
-    </h5>
-{{--    {{ $events.count() }}--}}
-</div>
+        @if (Request::is('akcie'))
+            <a href="{{ route('event.finished') }}">
+                Ukončené akcie
+            </a>
+        @else
+            <a href="{{ route('akcie.index') }}">
+                <i style="color: #3b32b3" class="fas fa-check"></i>
+                Aktívne akcie
+            </a>
+        @endif
 
+    </h5>
+    {{-- {{ $events.count() }} --}}
+</div>
