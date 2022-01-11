7a0e91072ef07d3d97b47ceda9db6afb01e046f0
Gabriel Gajdoš <gajdosgabo@gmail.com>
2022-01-11 09:13:09 +0100

    Icon profil menu

--- resources/views/components/icons/background.blade.php
+++ resources/views/components/icons/background.blade.php
@@ -1,5 +1,5 @@
 <a title="{{ $title }}" href="?posts={{ $requestValue }}">
-    <div class="flex hover:bg-gray-100 p-2 rounded-full h-9 w-9 items-center justify-center {{ $bgClass }}">
+    <div class="flex hover:bg-gray-100 p-2 rounded-full h-9 w-9 items-center justify-center {{ $bgClass() }}">
         {{ $slot }}
     </div>
 </a>
