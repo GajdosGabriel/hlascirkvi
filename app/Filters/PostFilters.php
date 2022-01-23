9918401f64f92d3ff16350e252312d496ca7ac5d
Gabriel Gajdoš <gajdosgabo@gmail.com>
2022-01-23 08:09:24 +0100

    Search component improve

--- app/Filters/PostFilters.php
+++ app/Filters/PostFilters.php
@@ -40,6 +40,7 @@ class PostFilters extends Filters
 
     public function search()
     {
+        session()->flash('search', $this->request->title);
         return $this->builder->where('title','LIKE','%'. $this->request->title .'%');
     }
 
--- resources/views/components/search-form.blade.php
+++ resources/views/components/search-form.blade.php
@@ -1,8 +1,16 @@
-<form action="{{ URL::current() }}" class="flex">
+<form action="{{ URL::current() }}" class="flex items-center">
+    
+    @if (session()->has('search'))
+        <a href="{{ URL::current() }}">
+            <div class="border-2 bg-gray-200 px-2 rounded-md  mr-4 cursor-pointer">
+                {{ session()->get('search') }} X
+            </div>
+        </a>
+    @endif
 
     <input name="posts" value="search" type="hidden" class="border-2 border-gray-500 px-2 rounded-md" />
-    <input name="title" class="border-2 border-gray-500 px-2 rounded-md" />
+    <input name="title" class="border-2 border-gray-500 px-2 rounded-md py-1" required
+        value="{{ session()->get('search') }}" />
 
     <button class="btn btn-small">Hľadať</button>
-
 </form>
