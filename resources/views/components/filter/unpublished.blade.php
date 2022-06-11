978bfec6121d7ed93d7ba9caf13bca8cd126f667
Gabriel Gajdoš <gajdosgabo@gmail.com>
2022-06-11 11:58:42 +0200

    Post filter uppublished II.

--- resources/views/components/filter/unpublished.blade.php
+++ resources/views/components/filter/unpublished.blade.php
@@ -1,7 +1,7 @@
 <a href="{{ URL::current() . '?posts=unpublished' }}">
-    <div @if (Request::has('unpublished')) class="bg-green-300 w-min px-2 rounded-md flex" @endif>
+    <div @if (Request::filled('posts') == 'unpublished') class="bg-green-300 w-min px-2 rounded-md flex" @endif>
         Nepublikované
-        @if (Request::has('unpublished'))
+        @if (Request::filled('posts') == 'unpublished')
             <a href="{{ URL::current() }}">
                 <div class="px-2 hover:text-gray-400">
                     X
