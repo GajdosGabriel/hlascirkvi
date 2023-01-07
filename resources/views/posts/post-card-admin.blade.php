fe887ea109188efb02830136c917798e0b068a9a
Gabriel Gajdoš <gajdosgabo@gmail.com>
2023-01-07 14:23:38 +0100

    Post card admin

---
+++ resources/views/posts/post-card-admin.blade.php
@@ -0,0 +1,48 @@
+<div class="border-2 border-gray-400 rounded-md shadow-md text-xs md:text-sm flex my-4">
+
+    <div style="max-height: 11rem; overflow: hidden;">
+        @if ($post->favorites()->count())
+            <div class="absolute bottom-0 right-0 bg-red-600 p-1 rounded-sm text-xs text-gray-200">
+                Doporúčené
+            </div>
+        @endif
+
+        @if ($post->video_duration)
+            <div class="absolute bottom-0 right-0 bg-gray-700 p-1 rounded-sm text-xs text-gray-200">
+                {{ $post->video_duration }}
+            </div>
+        @endif
+
+        <a href="{{ route('post.show', [$post->id, $post->slug]) }}">
+            @include('posts.image')
+        </a>
+    </div>
+
+
+
+    <div class="w-full p-2">
+
+        <a href="{{ route('post.show', [$post->id, $post->slug]) }}">
+            <h6 class="font-semibold" title="{{ $post->title }}">{{ Str::limit($post->title, 48) }}</h6>
+        </a>
+
+
+        <div class="flex space-x-5 ">
+            <time datetime="{{ $post->created_at }}">{{ $post->dateForHumans() }}</time>
+
+            <div class="flex">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd"
+                        d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
+                        clip-rule="evenodd" />
+                </svg>
+                {{ $post->comments()->count() }}
+            </div>
+
+            <div>Zobrazenia {{ $post->count_view }}</div>
+
+        </div>
+    </div>
+
+
+</div>
