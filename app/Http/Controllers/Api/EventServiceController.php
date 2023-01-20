48680389b2dd609d8db0ac12b446ff09de5679b2
Gabriel Gajdoš <gajdosgabo@gmail.com>
2023-01-20 14:02:47 +0100

    EventService newreload finish

--- app/Http/Controllers/Api/EventServiceController.php
+++ app/Http/Controllers/Api/EventServiceController.php
@@ -6,16 +6,26 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Event;
 use App\Services\Extractor\ExtractTkkbs;
+use App\Services\Extractor\ExtractVyveska;
+use App\Services\Extractor\ExtractEcav;
 
 class EventServiceController extends Controller
 {
     public function newReolad(Event $event) {
 
-        $event = (new ExtractTkkbs())->parseEvent($event->orginal_source, $event);
+        if($event->organization_id == 101 ) {
+            $event = (new ExtractTkkbs())->parseEvent($event->orginal_source, $event);
+        }
+
+        if($event->organization_id == 271 ) {
+            $event = (new ExtractVyveska())->parseEvent($event->orginal_source, $event);
+        }
+
+        if($event->organization_id == 102 ) {
+            $event = (new ExtractEcav())->parseEvent($event->orginal_source, $event);
+        }
 
         return back();
-
-       dd($event);
 
     }
 }
