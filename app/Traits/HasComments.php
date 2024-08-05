924124d85640362ebcabfe2b96ff592481a44831
Gabriel Gajdoš <gajdosgabo@gmail.com>
2024-08-05 12:35:49 +0200

    Fix old mistagies

--- app/Traits/HasComments.php
+++ app/Traits/HasComments.php
@@ -9,17 +9,17 @@ trait HasComments
 
     public function comments()
     {
-        return $this->morphMany(Comment::class, 'commentable')->with('organization');
+        return $this->morphMany(Comment::class, 'commentable')->with('user');
     }
 
     public function addComment($comment)
     {
         if (auth()->check()) {
-            $comment = $this->comments()->create(array_merge($comment, ['organization_id' => auth()->user()->org_id]));
+            $comment = $this->comments()->create(array_merge($comment, ['user_id' => auth()->user()->org_id]));
             return $comment;
         }
         // organization_id 100 in unknowle user for anonyms comments
-        $comment = $this->comments()->create(array_merge($comment, ['organization_id' => 100]));
+        $comment = $this->comments()->create(array_merge($comment, ['user_id' => 100]));
         $comment->delete();
 
         return $comment;
